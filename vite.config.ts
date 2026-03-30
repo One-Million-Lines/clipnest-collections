7c83ad1ccea3117ed481f14b0c989f88bfffbb8d
Alexandru Rada <alexandru.rada@gmail.com>
2026-03-30 21:20:05 +0200

    small changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/demo/clipnest-collections/",
+  base: mode === "production" ? "/demo/clipnest-collections/" : "/",
   server: {
     host: "::",
     port: 5301,
